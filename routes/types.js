213448904af2c58550a0c4912c5b7207bc855095
Muriel Bullot <murielbullot@hotmail.fr>
2025-12-21 18:54:22 +0100

    améliorations diverses

--- routes/types.js
+++ routes/types.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Types = require('../models/Types');
 
-// Ajout d'un nouvelenregistrement dans la base de données.
+// Ajout d'un nouvel enregistrement dans la base de données.
 router.post('/', async (req, res) => {
     try {
         const newType = new Types(req.body);
         const typeRegistered = await newType.save();
+        await typeRegistered.populate('affinites', 'nom description');
         res.status(200).json(typeRegistered);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -15,7 +16,21 @@ router.post('/', async (req, res) => {
 // Obtenir un tableau JSON contenant tous les enregistrements de la table.
 router.get('/', async (req, res) => {
     try {
-        const type = await Types.find();
+        const type = await Types.find().populate('affinites', 'nom description');
+        res.status(200).json(type);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+// Trouver un enregistrement par ID.   
+router.get('/:id', async (req, res) => {
+    try {
+        const type = await Types.findById(req.params.id).populate('affinites', 'nom description');
+
+          if (!type) {
+            return res.status(404).json({ error: 'Objet non trouvé' });
+        }
         res.status(200).json(type);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -25,7 +40,15 @@ router.get('/', async (req, res) => {
 // Modifier un enregistrement
 router.put('/:id', async (req, res) => {
     try {
-        const updatedType = await Types.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        const updatedType = await Types.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            { new: true }
+        ).populate('affinites', 'nom description');
+
+        if (!updatedType) {
+            return res.status(404).json({ error: 'Type non trouvé' });
+        }
         res.status(200).json(updatedType);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -36,6 +59,9 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     try {
         const deletedType = await Types.findByIdAndDelete(req.params.id);
+        if (!deletedType) {
+            return res.status(404).json({ error: 'Type non trouvé' });
+        }
         res.status(200).json(deletedType);
     } catch (error) {
         res.status(400).json({ error: error.message });
